020890343b18c56812b0e2e58a4154ff643e3810
Mohamed Hamza <mohamed.hamza@se.linedata.com>
2023-08-24 00:22:22 +0100

    fix categories and products

--- src/components/Home/Content/Categories.jsx
+++ src/components/Home/Content/Categories.jsx
@@ -8,32 +8,41 @@ const Categories = () => {
         omnis et dignissimos neque delectus odit, quo laboriosam vitae facilis?
       </p>
       <div className="home-categories__images">
-        <div className="home-categories__images--item">
-          <img
-            src="https://images.pexels.com/photos/4352247/pexels-photo-4352247.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt="img1"
-          />
+        <div className="home-categories__images--container">
+          <div className="home-categories__images--container--item">
+            <img
+              src="https://images.pexels.com/photos/4352247/pexels-photo-4352247.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="img1"
+            />
+          </div>
           <h2>Mobilier</h2>
         </div>
-        <div className="home-categories__images--item">
-          <img
-            src="https://images.pexels.com/photos/3932929/pexels-photo-3932929.jpeg?auto=compress&cs=tinysrgb&w=600"
-            alt="img2"
-          />
+
+        <div className="home-categories__images--container">
+          <div className="home-categories__images--container--item">
+            <img
+              src="https://images.pexels.com/photos/3932929/pexels-photo-3932929.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt="img2"
+            />
+          </div>
           <h2>Accessoires</h2>
         </div>
-        <div className="home-categories__images--item">
-          <img
-            src="https://images.pexels.com/photos/4825701/pexels-photo-4825701.jpeg?auto=compress&cs=tinysrgb&w=600"
-            alt="img3"
-          />
+        <div className="home-categories__images--container">
+          <div className="home-categories__images--container--item">
+            <img
+              src="https://images.pexels.com/photos/4825701/pexels-photo-4825701.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt="img3"
+            />
+          </div>
           <h2>Tentes & Abris</h2>
         </div>
-        <div className="home-categories__images--item">
-          <img
-            src="https://images.pexels.com/photos/5825527/pexels-photo-5825527.jpeg?auto=compress&cs=tinysrgb&w=600"
-            alt="img4"
-          />
+        <div className="home-categories__images--container">
+          <div className="home-categories__images--container--item">
+            <img
+              src="https://images.pexels.com/photos/5825527/pexels-photo-5825527.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt="img4"
+            />
+          </div>
           <h2>Cloisons</h2>
         </div>
       </div>
