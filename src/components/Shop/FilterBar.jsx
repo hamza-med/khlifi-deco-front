4e49c20a3d7b5cf88656250bad7fb5f3b7a2706f
Mohamed Hamza <mohamed.hamza@se.linedata.com>
2023-08-28 13:27:48 +0100

    add cards and filters

--- src/components/Shop/FilterBar.jsx
+++ src/components/Shop/FilterBar.jsx
@@ -1,8 +1,7 @@
+import { Select } from "@chakra-ui/react";
 import { BsGridFill, BsViewList } from "react-icons/bs";
 
-const FilterBar = ({setDisplay}) => {
-  
-
+const FilterBar = ({ setDisplay }) => {
   return (
     <div className="filter-bar__container">
       <div className="filter-bar__container__content">
@@ -16,16 +15,16 @@ const FilterBar = ({setDisplay}) => {
 
       <div className=" filter-bar__container__inputs">
         <div className="show_input">
-          <span>Montrer</span>
-          <input type="number" placeholder="16" />
+          <label htmlFor="number-input">Montrer</label>
+          <input type="number" placeholder="16" id="number-input" />
         </div>
         <div className="filter_input">
-          <span>Trier par</span>
-          <select type="text">
+          <label htmlFor="standard-select">Trier par</label>
+          <Select htmlFor="standard-select">
             <option value="">Nom</option>
             <option value="">Prix, croissant</option>
-            <option value="">Prix, décroissant</option>
-          </select>
+            <option value="">Prix, décroissant</option>          
+          </Select>
         </div>
       </div>
     </div>
--- src/components/Shop/ShopProducts.jsx
+++ src/components/Shop/ShopProducts.jsx
@@ -2,16 +2,35 @@ import ProductCard from "@/uilib/ProductCard";
 
 const ShopProducts = ({ display }) => {
   return (
-    <div className="showProducts_container">
+    <div className={`showProducts_container ${display}`
+}>
       <div className="showProducts_container__left">
         <p className="title">Mobilier</p>
         <div className="content">
-          <span>Chaises</span>
-          <span>Fauteuils</span>
-          <span>Canapés</span>
-          <span>Poufs</span>
-          <span>Tables</span>
-          <span>Bars</span>
+          <div className="content__input">
+            <input type="checkbox" value="checked" />
+            <span>Chaises</span>
+          </div>
+          <div className="content__input">
+            <input type="checkbox" />
+            <span>Fauteuils</span>
+          </div>
+          <div className="content__input">
+            <input type="checkbox" />
+            <span>Canapés</span>
+          </div>
+          <div className="content__input">
+            <input type="checkbox" />
+            <span>Poufs</span>
+          </div>
+          <div className="content__input">
+            <input type="checkbox" />
+            <span>Tables</span>
+          </div>
+          <div className="content__input">
+            <input type="checkbox" />
+            <span>Bars</span>
+          </div>
         </div>
       </div>
       <div className={`showProducts_container__right ${display}`}>
@@ -27,7 +46,6 @@ const ShopProducts = ({ display }) => {
         <ProductCard display={display} />
         <ProductCard display={display} />
         <ProductCard display={display} />
-        
       </div>
     </div>
   );
