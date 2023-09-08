be09eeb7a0c4772c16b89b8620f70c6892ec0d4d
Mohamed Hamza <mohamed.hamza@se.linedata.com>
2023-09-08 14:24:31 +0100

    add related products page

---
+++ src/components/ProductDetail/RelatedProducts.jsx
@@ -0,0 +1,48 @@
+import useFetch from "@/hooks/useFetch";
+import ProductCard from "@/uilib/ProductCard";
+import { useState } from "react";
+
+const RelatedProducts = (props) => {
+  const [pageSize, setPageSize] = useState(4);
+
+  const { data: products, meta } = useFetch(
+    `/products?pagination[pageSize]=${pageSize}&pagination[page]=1&[filters][sub_categories][id][$eq]=${props.subCatId}&[filters][id][$ne]=${props.prodId}&populate=*`
+  );
+
+  const handleClick = () => {
+    setPageSize((prev) => prev + 4);
+  };
+  const handleClick2 = () => {
+    setPageSize((prev) => prev - 4);
+  };
+  return (
+    <div className="home-products product-detail">
+      <h1 className="home-products__title product-detail-title">
+        Produits apparentés
+      </h1>
+      <div className="home-products__products">
+        {products?.map((product) => {
+          return (
+            <ProductCard
+              product={product?.attributes}
+              key={product?.id}
+              id={product?.id}
+            />
+          );
+        })}
+      </div>
+      {pageSize < meta?.pagination.total && (
+        <button className="home-products__button" onClick={handleClick}>
+          afficher plus
+        </button>
+      )}
+      {pageSize > meta?.pagination.total && (
+        <button className="home-products__button" onClick={handleClick2}>
+          afficher moins
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default RelatedProducts;
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,12 +1,13 @@
 import LinksBar from "@/components/ProductDetail/LinksBar";
 import ProductInfo from "@/components/ProductDetail/ProductInfo";
+import RelatedProducts from "@/components/ProductDetail/RelatedProducts";
 import useFetch from "@/hooks/useFetch";
 import { useParams } from "react-router-dom";
 
 const ProductDetail = () => {
   const { productId } = useParams();
   const { data: product } = useFetch(`/products/${productId}?populate=*`);
-
+  
   return (
     <div>
       <LinksBar
@@ -15,6 +16,10 @@ const ProductDetail = () => {
         subData={product?.attributes?.sub_categories?.data[0]}
       />
       <ProductInfo prodData={product?.attributes} prodId={product?.id} />
+      <RelatedProducts
+        prodId={product?.id}
+        subCatId={product?.attributes?.sub_categories?.data[0]?.id}
+      />
     </div>
   );
 };
