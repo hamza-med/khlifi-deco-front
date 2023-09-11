9a718b5bb024a3aa1986a19f4bd40122a7681aff
Mohamed Hamza <mohamed.hamza@se.linedata.com>
2023-09-11 09:04:53 +0100

    fix scrolling when opening menu

--- src/components/Layout/Header/ToggleMenu.jsx
+++ src/components/Layout/Header/ToggleMenu.jsx
@@ -2,10 +2,13 @@ import { LuMenu, LuX } from "react-icons/lu";
 const ToggleMenu = ({ showDrawer, isOpen }) => {
   const openDrawer = () => {
     showDrawer((prev) => !prev);
+    const root = document.getElementById("root");
     if (typeof window != "undefined" && window.document && !isOpen) {
       document.body.style.overflow = "hidden";
+      root.style.overflow = "hidden";
     } else {
       document.body.style.overflow = "unset";
+      root.style.overflow = "unset";
     }
   };
   return (
