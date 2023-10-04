2e6c66a079e340951c51040b679fe23a1b3dc98f
hamza-med <hamzaa.mohamed1998@gmail.com>
2023-10-04 10:22:38 +0100

    add condition to download pdf file

--- src/components/Dashboard/Header.jsx
+++ src/components/Dashboard/Header.jsx
@@ -11,7 +11,6 @@ const PickerInput = forwardRef(({ value, onClick, ...props }, ref) => (
       {...props}
       _hover={{ bgColor: "black", color: "white" }}
       p="1.5em 2em"
-      isLoading={props.loading}
       disabled={props.loading}
       bgColor="white"
       onClick={onClick}
@@ -30,15 +29,22 @@ const Header = ({ startDate, setStartDate }) => {
 
   const handleExportToPDF = async () => {
     setLoading(true);
-    const input = document.getElementById("file-to-export"); // Replace 'table-to-export' with the ID of your table
+    const input = document.getElementById("file-to-export");
     html2canvas(input).then((canvas) => {
       setLoading(false);
       let imgWidth = 208;
       let imgHeight = (canvas.height * imgWidth) / canvas.width;
-      const imgData = canvas.toDataURL("img/png");
+      const imgData = canvas.toDataURL("image/png");
       const pdf = new jsPDF("p", "mm", "a4");
       pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
-      pdf.save("devi.pdf");
+      const pdfHeight = pdf.internal.pageSize.getHeight();
+      const isFullCoverage = imgHeight > pdfHeight;
+      if (isFullCoverage) {
+        console.log("HTML content covers entire PDF");
+      } else {
+        console.log("HTML content does not cover entire PDF");
+        pdf.save("devi.pdf");
+      }
     });
   };
 
