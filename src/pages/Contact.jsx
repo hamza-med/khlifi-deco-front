94cf9b67fcb2995c10971d7096c0676b594b6642
hamza-med <hamza.mohamed1998@gmail.com>
2024-05-15 09:51:19 +0100

    add contact page and components

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import ContactCard from "@/components/Contact/ContactCard";
+import ContactForm from "@/components/Contact/ContactForm";
+
+const Contact = () => {
+  return (
+    <div className="contact_wrapper">
+      <ContactCard />
+      <ContactForm />
+    </div>
+  );
+};
+
+export default Contact;
