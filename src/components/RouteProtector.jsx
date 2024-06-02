6a18f47217b6ea573a32b4052476980e383956c6
hamza-med <hamza.mohamed1998@gmail.com>
2024-06-02 20:42:51 +0100

    add fix the dashboard

--- src/components/RouteProtector.jsx
+++ src/components/RouteProtector.jsx
@@ -1,13 +1,12 @@
 import { getLocalStorageItem } from "@/utils/localStorage";
 import { Navigate, useLocation, Outlet } from "react-router-dom";
-
+const allowedUsers = ["utilisation13@gmail.com", "youssefkhlifi18@gmail.com"];
 const RouteProtector = () => {
   let location = useLocation();
   let user = getLocalStorageItem("user");
   return !user?.id ? (
     <Navigate to="/login" state={{ from: location }} replace />
-  ) : user?.email !== "utilisation13@gmail.com" ||
-    user?.email !== "youssefkhlifi18@gmail.com" ? (
+  ) : !allowedUsers.includes(user?.email) ? (
     <Navigate to="/" state={{ from: location }} replace />
   ) : (
     <Outlet />
