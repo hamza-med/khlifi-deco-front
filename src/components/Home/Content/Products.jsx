d438612e2f4a460675d438205ed41ad11a222a27
hamza-med <hamza.mohamed1998@gmail.com>
2024-08-04 12:46:26 +0100

    add pack section to home page

--- src/components/Home/Content/Products.jsx
+++ src/components/Home/Content/Products.jsx
@@ -1,55 +1,44 @@
-import useFetch from "@/hooks/useFetch";
 import { Skeleton } from "@chakra-ui/react";
 import { lazy, Suspense } from "react";
-import { forwardRef, useState } from "react";
+import { forwardRef } from "react";
 import { useTranslation } from "react-i18next";
 const ProductCard = lazy(() => import("@/uilib/ProductCard"));
 
-const Products = forwardRef((props, ref) => {
-  const [pageSize, setPageSize] = useState(8);
-  const {
-    i18n: { language },
-  } = useTranslation();
-  const { data: products, meta } = useFetch(
-    `/products?locale=${language}&pagination[pageSize]=${pageSize}&pagination[page]=1&populate[img][fields][0]=name&populate[img][fields][1]=url`
-  );
-  const { t } = useTranslation();
-  const { prodTitle, prodBtnPlus, prodBtnLess } = t("home");
-  const handleClick = () => {
-    setPageSize((prev) => prev + 8);
-  };
-  const handleClick2 = () => {
-    setPageSize((prev) => prev - 8);
-  };
-  return (
-    <div className="home-products" ref={ref}>
-      <h1 className="home-products__title">{prodTitle}</h1>
-      <div className="home-products__products">
-        {products?.map((product) => {
-          return (
-            <Suspense
-              key={product?.id}
-              fallback={
-                <Skeleton height={["350px", "400px"]} width={["250px"]} />
-              }
-            >
-              <ProductCard product={product?.attributes} id={product?.id} />
-            </Suspense>
-          );
-        })}
+const Products = forwardRef(
+  ({ title, handleClick, handleClick2, meta, products,pageSize }, ref) => {
+    const { t } = useTranslation();
+    const { prodBtnPlus, prodBtnLess } = t("home");
+
+    return (
+      <div className="home-products" ref={ref}>
+        <h1 className="home-products__title">{title}</h1>
+        <div className="home-products__products">
+          {products?.map((product) => {
+            return (
+              <Suspense
+                key={product?.id}
+                fallback={
+                  <Skeleton height={["350px", "400px"]} width={["250px"]} />
+                }
+              >
+                <ProductCard product={product?.attributes} id={product?.id} />
+              </Suspense>
+            );
+          })}
+        </div>
+        {pageSize < meta?.pagination.total && (
+          <button className="home-products__button" onClick={handleClick}>
+            {prodBtnPlus}
+          </button>
+        )}
+        {pageSize > meta?.pagination.total && (
+          <button className="home-products__button" onClick={handleClick2}>
+            {prodBtnLess}
+          </button>
+        )}
       </div>
-      {pageSize < meta?.pagination.total && (
-        <button className="home-products__button" onClick={handleClick}>
-          {prodBtnPlus}
-        </button>
-      )}
-      {pageSize > meta?.pagination.total && (
-        <button className="home-products__button" onClick={handleClick2}>
-          {prodBtnLess}
-        </button>
-      )}
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default Products;
--- src/components/Home/Content/index.jsx
+++ src/components/Home/Content/index.jsx
@@ -1,10 +1,31 @@
-import { Suspense, lazy } from "react";
+import useFetch from "@/hooks/useFetch";
+import { Suspense, lazy, useState } from "react";
+import { useTranslation } from "react-i18next";
 
 const Categories = lazy(() => import("./Categories"));
 const ExploreSection = lazy(() => import("./ExploreSection"));
 const Products = lazy(() => import("./Products"));
 
 const Content = ({ productsRef }) => {
+  const [pageSize, setPageSize] = useState(8);
+  const {
+    i18n: { language },
+  } = useTranslation();
+  const { data: products, meta } = useFetch(
+    `/products?locale=${language}&pagination[pageSize]=${pageSize}&pagination[page]=1&populate[img][fields][0]=name&populate[img][fields][1]=url&filters[categories][id][$ne]=${21}`
+  );
+  const { data: packs, meta: packsMeta } = useFetch(
+    `/products?locale=${language}&pagination[pageSize]=${pageSize}&pagination[page]=1&populate[img][fields][0]=name&populate[img][fields][1]=url&filters[categories][id][$eq]=${21}&populate[categories][fields][0]=title`
+  );
+  const { t } = useTranslation();
+  const { prodTitle, packTitle } = t("home");
+  const handleClick = () => {
+    setPageSize((prev) => prev + 8);
+  };
+  const handleClick2 = () => {
+    setPageSize((prev) => prev - 8);
+  };
+
   return (
     <>
       <div className="home-container">
@@ -12,7 +33,26 @@ const Content = ({ productsRef }) => {
           <Categories />
         </Suspense>
         <Suspense>
-          <Products ref={productsRef} />
+          <Products
+            pageSize={pageSize}
+            ref={productsRef}
+            title={prodTitle}
+            products={products}
+            meta={meta}
+            handleClick={handleClick}
+            handleClick2={handleClick2}
+          />
+          {packs?.length > 0 && (
+            <Products
+              pageSize={pageSize}
+              ref={productsRef}
+              title={packTitle}
+              products={packs}
+              meta={packsMeta}
+              handleClick={handleClick}
+              handleClick2={handleClick2}
+            />
+          )}
         </Suspense>
       </div>
       <Suspense>
